22158c84737764ea718631ff650342175d81ca88
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2021-09-28 10:29:41 +0100

    feature/HMA-4886 - Dependency version updates

--- test/uk/gov/hmrc/mobilepayments/common/BaseSpec.scala
+++ test/uk/gov/hmrc/mobilepayments/common/BaseSpec.scala
@@ -17,21 +17,20 @@
 package uk.gov.hmrc.mobilepayments.common
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import eu.timepit.refined.auto._
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 import play.api.test.DefaultAwaitTimeout
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.mobilepayments.domain.types.ModelTypes.JourneyId
 
 import scala.concurrent.ExecutionContext
 
-trait BaseSpec extends WordSpec with MockFactory with Matchers with DefaultAwaitTimeout {
+trait BaseSpec extends AnyWordSpec with MockFactory with Matchers with DefaultAwaitTimeout {
   implicit lazy val ec:           ExecutionContext  = scala.concurrent.ExecutionContext.Implicits.global
   implicit lazy val hc:           HeaderCarrier     = HeaderCarrier()
   implicit lazy val system:       ActorSystem       = ActorSystem()
-  implicit lazy val materializer: ActorMaterializer = ActorMaterializer()
 
   val journeyId: JourneyId = "13345a9d-0958-4931-ae83-5a36e4ccd979"
 }
