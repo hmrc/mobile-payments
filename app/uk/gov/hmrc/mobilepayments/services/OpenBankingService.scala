75dbd34282395eac9132538e660e0fc76ef77c71
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2021-11-02 12:07:30 +0000

    bug/fix-banks-response-type - Changing response structure from OB banks list

--- app/uk/gov/hmrc/mobilepayments/services/OpenBankingService.scala
+++ app/uk/gov/hmrc/mobilepayments/services/OpenBankingService.scala
@@ -34,7 +34,7 @@ class OpenBankingService @Inject() (
     journeyId:                 JourneyId
   )(implicit executionContext: ExecutionContext,
     headerCarrier:             HeaderCarrier
-  ): Future[BanksResponse] = connector.getBanks(journeyId)
+  ): Future[BanksResponse] = connector.getBanks(journeyId).map(banks => BanksResponse(banks))
 
   def initiatePayment(
     amount:                 Long,
