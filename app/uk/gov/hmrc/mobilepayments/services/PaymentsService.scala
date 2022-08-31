d49679b5b9bc8adb578487fbc92fdbcb8e582083
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2022-08-31 11:54:24 +0100

    [HMA-6072] Return sessionId in pay-by-card response

--- app/uk/gov/hmrc/mobilepayments/services/PaymentsService.scala
+++ app/uk/gov/hmrc/mobilepayments/services/PaymentsService.scala
@@ -64,7 +64,7 @@ class PaymentsService @Inject() (connector: PaymentsConnector) {
       .map(response =>
         PayByCardResponse(
           response.urlWithoutDomainPrefix,
-          headerCarrier.sessionId.map(_.value).getOrElse(throw new NotFoundException("Session ID not found"))
+          journeyId.value
         )
       )
 
