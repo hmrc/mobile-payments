3be6ad2193f89a45fcc845139025e806a9d87ae7
pdhiman3006 <176318726+pdhiman3006@users.noreply.github.com>
2025-08-11 12:08:04 +0100

    HMA-10243 it test case fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "10.1.0"
+  private val bootstrapPlayVersion = "9.11.0"
   private val playHmrcApiVersion = "8.2.0"
   private val refinedVersion = "0.11.3"
   private val domainVersion = "12.1.0"
